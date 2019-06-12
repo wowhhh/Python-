962a3539123f306b305bbac9ac20a5cb7b9c9dd3
wyb <1792195840@qq.com>
2019-06-12 18:20:57 +0800

    2019--06-12 面向对象开始 ByWyb

---
+++ 2_OOP/OOP_01.py
@@ -0,0 +1,28 @@
+'''
+定义一个学生类，用来形容学生
+'''
+# 定义一个空的类
+class Student():
+    # 一个空类，pass代表直接跳过，不用pass则会报错的
+    pass
+
+# 定义一个对象
+wangyibo = Student()
+
+#
+class PythonStudent():
+    name = None  # 姓名属性 ,用None占位，必须赋值的
+    age = 18 # 值
+    course = "Python"
+
+# 注意
+    # 1：缩进层级
+    # 2：self
+    def DoHomework(self): # 自动生成了self
+        print("在做作业！")
+        return None # 推荐在函数末尾使用return语句
+
+wangyibo2 = PythonStudent()
+wangyibo2.name = 'wyb'
+print(wangyibo2.name)
+wangyibo2.DoHomework()
