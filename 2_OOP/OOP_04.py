784bb415b1ab2373e2f523acf1fbed4f53ec130a
wyb <1792195840@qq.com>
2019-06-25 17:37:00 +0800

    面向对象

--- 2_OOP/OOP_04.py
+++ 2_OOP/OOP_04.py
@@ -11,3 +11,29 @@ class C(B,A):
 print(A.__mro__) #(<class '__main__.A'>, <class 'object'>)
 print(B.__mro__) #(<class '__main__.B'>, <class '__main__.A'>, <class 'object'>)
 print(C.__mro__) # (<class '__main__.C'>, <class '__main__.B'>, <class '__main__.A'>, <class 'object'>)
+
+class Fish():
+    def __init__(self,name):
+        self.name = name
+    def swim(self):
+        print("游泳ing")
+
+class Bird():
+    def __init__(self,name):
+        self.name = name
+    def fly(self):
+        print("飞ing")
+
+class Person():
+    def __init__(self,name):
+        self.name = name
+    def work(self):
+        print("干活ing")
+
+class SuperMan(Person,Bird,Fish):
+    pass
+
+s  = SuperMan("wyb")
+s.fly()
+s.swim()
+s.work()
