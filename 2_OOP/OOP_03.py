21cd9f236197e4dfc93cc50c9e870bf4fe0fdd39
wyb <1792195840@qq.com>
2019-06-25 11:16:38 +0800

    面向对象

--- 2_OOP/OOP_03.py
+++ 2_OOP/OOP_03.py
@@ -7,13 +7,18 @@ class Person():
     _petname = "sec" # 小名，是傲虎的，子类可以用，不可以公用
     def sleep(self):
         print("Sleeping ....")
-
+    def work(self):
+        print("赚钱")
 # 父类写在括号里面
 class Teacher(Person):
     pass
     def makeTest(self):
         print("在考试")
-
+    def work(self): #Person父类中存在work这个方法
+        self.makeTest()    # 增加功能
+        #扩充父类的功能只需要调用父类相同的函数
+        Person.work(self)  # 方法1
+        super().work()     # 方法2
 
 t = Teacher()
 print(t.name)  # 输出 ： wyb
@@ -21,4 +26,33 @@ print(t.makeTest())
 # 受保护访问
 print(t._petname)
 # 私有访问
-print(t.__score) #'Teacher' object has no attribute '__score'
+#print(t.__score) #'Teacher' object has no attribute '__score'
+
+
+'''构造函数'''
+class Dog():
+    # __init__ 就是构造函数
+    # 每次实例化的时候，第一个被调用
+    # 主要工作室进行初始化
+    def __init__(self):
+        print("构造函数")
+
+kaka = Dog()
+'''继承中的构造函数'''
+class Animal():
+    pass
+
+class PaxingAni(Animal):
+    pass
+    def __init__(self):
+        print("爬行动物的构造函数")
+    def __init__(self,name):  #构造函数含有参数
+        print(name,"的构造函数")
+
+class Cat(PaxingAni):
+    def __init__(self):
+        print("猫猫的构造函数")
+# 此处不写构造函数
+class ele(PaxingAni):
+    pass
+e = ele("wyb") # 那么调用的时候，ele里面没有构造函数，会去寻找父类里面的构造函数
